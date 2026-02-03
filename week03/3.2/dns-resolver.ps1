cc6ba855af3884832ef6e9e95e45f8b5e6cc21e0
LeoB1um <leo.blum@mymail.champlain.edu>
2026-02-02 23:33:50 -0500

    Homework 3.2

---
+++ week03/3.2/dns-resolver.ps1
@@ -0,0 +1,14 @@
+﻿param(
+    [string]$prefix,
+    [string]$dns
+)
+
+for ($i = 1; $i -le 255; $i++) {
+    $resolved = Resolve-DnsName -DnsOnly "$prefix.$i" -Server $dns -ErrorAction Ignore
+    if ($resolved) {
+        $namehost = $resolved.NameHost
+        if ($namehost) {
+            Write-Output "$prefix.$i $namehost"
+        }
+    }
+}
